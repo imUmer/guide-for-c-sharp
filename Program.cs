51aa688e46a3e2e01247d73d83350e96b5239245
Umer Niaz <57424391+imUmer@users.noreply.github.com>
2023-01-17 12:58:38 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a = 18;
+      int b = 6;
+      int c = a + b;
+      Console.WriteLine(c);
+    }
+  }
+}
