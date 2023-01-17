fe660014d0bfbc029b4425a43ba6398a1a8a9498
Umer Niaz <umer.niaz@Northbay.ISB>
2023-01-17 19:11:14 +0500

    branches and loops

--- NumberTask/Program.cs
+++ NumberTask/Program.cs
@@ -68,7 +68,78 @@ namespace HelloWorld
             decimal d = 3.0M;
             Console.WriteLine(c / d);
         }
+        /// Explore integer precision and limits
+        //// Getting Quotient and remainder
+        public void decisions(int a, int b)
+        {
+                int a = 5;
+                int b = 4;
+                int c = 10;
+                if (a + b < 10)
+                    Console.WriteLine("The answer is less than 10.");
 
+                //Make if and else work together
+                if (a + b > 10)
+                    Console.WriteLine("The answer is greater than 10");
+                else
+                    Console.WriteLine("The answer is not greater than 10");
+
+                //Make if and else work with multiple condition using AND OR operators
+                if (a + b > 10 && c==10 || b < a)
+                    Console.WriteLine("The answer is greater than 10");
+                else
+                    Console.WriteLine("The answer is not greater than 10");
+
+        }
+        /// LOOPS 
+        public void loops(int a, int b)
+        {
+                int counter = 0;
+                int length = 5;
+                Console.WriteLine($"The For loop counter");
+                for (int i = 0; i < length; i++)
+                {
+                    Console.WriteLine($"The counter is {counter}");
+                    counter++;
+                }
+
+                Console.WriteLine($"The while loop counter");
+                while (counter < 10)
+                {
+                    Console.WriteLine($"The do-while loop counter");
+                    counter++;
+                }
+
+                Console.WriteLine($"The counter is {counter}");
+                do
+                {
+                    Console.WriteLine($"The counter is {counter}");
+                    counter++;
+                } while (counter < 10);
+
+                // nested loops
+                Console.WriteLine($"The Nested loop row and columns");
+                for (int r = 0; r < length; r++)
+                {
+                    for (int c = 0; c < length; c++)
+                    {
+                        Console.WriteLine($"|{r}| + |{c}|");
+                    }
+                }
+
+                // Task :
+                int sum = 0;
+                for (int n = 1; n < 21; n++)
+                {
+                if (n % 3 == 0)
+                {
+                    sum +=  n;
+                }
+                }
+                Console.WriteLine($"The sum is {sum}");
+        }
+
+     
 
     }
 
